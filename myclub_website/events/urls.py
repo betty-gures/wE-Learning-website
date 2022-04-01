34f848490db3f4e8f493228c6e31ba21cda9d25f
Betul Gures <fatma.betul.gures@gmail.com>
2022-04-01 21:46:11 +0300

    Initialisation of the project
    From this commit on, the tutorial on https://www.youtube.com/watch?v=Rpi0Ne1nMdk&list=PLPSM8rIid1a3TkwEmHyDALNuHhqiUiU5A&index=2 will be followed.

--- myclub_website/events/urls.py
+++ myclub_website/events/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    
+    #path('', views.home, name="home"),
+    path('<int:year>/<str:month>/', views.home, name="home")
 ]
--- myclub_website/events/views.py
+++ myclub_website/events/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
-def home(request):
-	return render (request, 'home.html', {})
+def home(request, year, month):
+	name = "Betul"
+	return render (request, 'home.html', {"name":name,"year":year,"month":month })
