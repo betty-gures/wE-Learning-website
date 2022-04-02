6b6d8229a133ff1c37bb9f5443a2ee57c67aae1a
Betul Gures <fatma.betul.gures@gmail.com>
2022-04-02 14:46:47 +0300

    Setup of MySQL as a database
    Successfully removed the default sqlite and configured MySQL instead.

--- myclub_website/myclub_website/settings.py
+++ myclub_website/myclub_website/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'myclub_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test1',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': '',
     }
 }
 
