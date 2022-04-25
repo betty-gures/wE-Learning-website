0b4d5b2cea55b091a3547d270d441f11e128f7d4
Betul Gures <fatma.betul.gures@gmail.com>
2022-04-25 23:16:41 +0300

    Post system implemented
    The posts that were created on the backend, are now visible on http://localhost:8000/social/

--- myclub_website/social/views.py
+++ myclub_website/social/views.py
@@ -5,11 +5,12 @@ from .models import Post
 class PostListView(View):
 	def get(self, request, *args, **kwargs):
 		posts = Post.objects.all().order_by('-created_on')
+		
 
 		context = { 
 			'post_list': posts,
 		}
 
-		return render (request, 'social/post_list.html, context')
+		return render (request, 'social/post_list.html', context)
 
 # Create your views here.
