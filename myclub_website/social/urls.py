e3be7ff9c9854cddb86202043c0f6095b5883724
Betul Gures <fatma.betul.gures@gmail.com>
2022-04-26 01:54:42 +0300

    Posts have details
    As we click on one post, it takes us to a separate page, created for the private key of that post, to display additional details.

--- myclub_website/social/urls.py
+++ myclub_website/social/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
 	path('', PostListView.as_view(), name='post-list'),
+	path('post/<int:pk>', PostDetailView.as_view(),name='post-detail'),
 	
 ]
--- myclub_website/social/views.py
+++ myclub_website/social/views.py
@@ -32,3 +32,13 @@ class PostListView(View):
 		}
 		return render (request, 'social/post_list.html', context)
 
+class PostDetailView(View):
+	def get(self,request,pk, *args, **kwargs):
+		post = Post.objects.get(pk=pk)
+
+		context = {
+			'post':post
+
+		}
+
+		return render (request, 'social/post_detail.html',context)
