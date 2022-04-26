6a28b5da8a780c8dc17a11618f7ecb3036c27c9a
Betul Gures <fatma.betul.gures@gmail.com>
2022-04-26 04:24:45 +0300

    Edit Post feature added

--- myclub_website/social/urls.py
+++ myclub_website/social/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostEditView
 
 urlpatterns = [
 	path('', PostListView.as_view(), name='post-list'),
 	path('post/<int:pk>', PostDetailView.as_view(),name='post-detail'),
-	
+	path('post/edit/<int:pk>', PostEditView.as_view(),name='post-edit'),
 ]
--- myclub_website/social/views.py
+++ myclub_website/social/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View
 from .models import Post
 from .forms import PostForm, CommentForm
+from django.views.generic.edit import UpdateView, DeleteView
 
 class PostListView(View):
 	def get(self, request, *args, **kwargs):
@@ -45,3 +47,12 @@ class PostDetailView(View):
 		}
 
 		return render (request, 'social/post_detail.html',context)
+
+class PostEditView(UpdateView):
+	model = Post
+	fields = ['body']
+	template_name = 'social/post_edit.html'
+
+	def get_success_url(self):
+		pk = self.kwargs['pk']
+		return reverse_lazy('post-detail',kwargs={'pk':pk})
