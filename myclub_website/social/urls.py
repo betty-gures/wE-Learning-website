2665d55401da7973dcb690131750223e08dff103
Betul Gures <fatma.betul.gures@gmail.com>
2022-05-03 15:46:50 +0300

    Follow and Unfollow functionality implemented

--- myclub_website/social/urls.py
+++ myclub_website/social/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView, ProfileView, ProfileEditView, AddFollower
+from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView, ProfileView, ProfileEditView, AddFollower, RemoveFollower
 
 urlpatterns = [
 	path('', PostListView.as_view(), name='post-list'),
@@ -10,6 +10,7 @@ urlpatterns = [
 	path('profile/<int:pk>/', ProfileView.as_view(),name='profile'),
 	path('profile/edit/<int:pk>/', ProfileEditView.as_view(),name='profile-edit'),
 	path('profile/<int:pk>/followers/add', AddFollower.as_view(), name='add-follower'),
-	
+	path('profile/<int:pk>/followers/remove', RemoveFollower.as_view(), name='remove-follower'),
+
 
 ]
--- myclub_website/social/views.py
+++ myclub_website/social/views.py
@@ -114,10 +114,27 @@ class ProfileView(View):
 		profile = UserProfile.objects.get(pk=pk) 
 		user = profile.user 
 		posts = Post.objects.filter(author=user).order_by('-created_on')
+		
+		followers = profile.followers.all()
+
+		if len(followers) == 0:
+			is_following = False
+
+		for follower in followers:
+			if follower == request.user:
+				is_following = True
+				break
+			else:
+				is_following = False
+
+		number_of_followers = len (followers)
+
 		context={
 			'user':user,
 			'profile':profile,
-			'posts': posts
+			'posts': posts,
+			'number_of_followers': number_of_followers,
+			'is_following': is_following,
 
 		}
 		return render(request, 'social/profile.html',context)
@@ -142,3 +159,9 @@ class AddFollower(LoginRequiredMixin, View):
 
 		return redirect ('profile', pk=profile.pk)
 
+class RemoveFollower(LoginRequiredMixin, View):
+	def post(self,request,pk,*args,**kwargs):
+		profile = UserProfile.objects.get(pk=pk)
+		profile.followers.remove(request.user)
+
+		return redirect ('profile', pk=profile.pk)
