7597b4bb03c350eaefdc0d8a7349e006380490e2
Betul Gures <fatma.betul.gures@gmail.com>
2022-05-03 20:52:21 +0300

    Organisation of CSS style

--- myclub_website/myclub_website/settings.py
+++ myclub_website/myclub_website/settings.py
@@ -137,6 +137,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATICFILES_DIRS = [
+
+BASE_DIR / 'static'
+]
+
 STATIC_URL = 'static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
