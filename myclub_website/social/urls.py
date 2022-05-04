d6d04e7c7d5416d92791af8c2e13c6bfa04c1093
Betul Gures <fatma.betul.gures@gmail.com>
2022-05-04 10:34:36 +0300

    Display list of followers
    From the profile page of a user, one can display the followers list of that user

--- myclub_website/social/urls.py
+++ myclub_website/social/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView, ProfileView, ProfileEditView, AddFollower, RemoveFollower, AddLike, AddDislike, UserSearch
+from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView, ProfileView, ProfileEditView, AddFollower, RemoveFollower, AddLike, AddDislike, UserSearch, ListFollowers
 
 urlpatterns = [
 	path('', PostListView.as_view(), name='post-list'),
-	path('post/<int:pk>', PostDetailView.as_view(),name='post-detail'),
-	path('post/edit/<int:pk>', PostEditView.as_view(),name='post-edit'),
-	path('post/delete/<int:pk>', PostDeleteView.as_view(),name='post-delete'),
+	path('post/<int:pk>/', PostDetailView.as_view(),name='post-detail'),
+	path('post/edit/<int:pk>/', PostEditView.as_view(),name='post-edit'),
+	path('post/delete/<int:pk>/', PostDeleteView.as_view(),name='post-delete'),
 	path('post/<int:post_pk>/comment/delete/<int:pk>/', CommentDeleteView.as_view(),name='comment-delete'),
 	path('post/<int:pk>/like/', AddLike.as_view(),name='like'),
 	path('post/<int:pk>/dislike/', AddDislike.as_view(),name='dislike'),
 	path('profile/<int:pk>/', ProfileView.as_view(),name='profile'),
+	path('profile/<int:pk>/followers/', ListFollowers.as_view(), name='list-followers'),
 	path('profile/edit/<int:pk>/', ProfileEditView.as_view(),name='profile-edit'),
-	path('profile/<int:pk>/followers/add', AddFollower.as_view(), name='add-follower'),
-	path('profile/<int:pk>/followers/remove', RemoveFollower.as_view(), name='remove-follower'),
+	path('profile/<int:pk>/followers/add/', AddFollower.as_view(), name='add-follower'),
+	path('profile/<int:pk>/followers/remove/', RemoveFollower.as_view(), name='remove-follower'),
     path('search/', UserSearch.as_view(), name='profile-search'),
+
 ]
