bdcf8a27735546b15bfdef46e0a703fb537af0f4
Betul Gures <fatma.betul.gures@gmail.com>
2022-05-23 22:41:14 +0300

    Bugfix

--- myclub_website/myclub_website/settings.py
+++ myclub_website/myclub_website/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "django-insecure-j+^tgm622$7%6076=r)j=^6v^&d3oald#k&-0t9upn!%rxy&m7"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
