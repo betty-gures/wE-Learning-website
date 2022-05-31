a97842058a470327a52fba6d9ab76202100ec4c3
Betul Gures <fatma.betul.gures@gmail.com>
2022-05-31 14:55:59 +0300

    Tests
    Attempted to create tests for the system

--- myclub_website/colearning/tests.py
+++ myclub_website/colearning/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from colearning.models import Room
 
-# Create your tests here.
+
+class RoomTestCase(TestCase):
+
+    def setUp(self):
+        room = Room.objects.create(name="Oda1")
+
+    def test_room_name(self):
+        room = Room.objects.get(id=1)
+        self.assertEqual(room.name, room.name.capitalize())
--- myclub_website/social/tests.py
+++ myclub_website/social/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+# from django.test import TestCase
+# from courses.models import Courses
+# from social.models import Post
+#
+#
+# class PostTestCase(TestCase):
+#
+#     def setUp(self):
+#         post = Post.objects.create(body="Hello weLearning!", learning_environment_id=1,
+#                                    author_id=1)
+#
+#     def test_learning_environment(self):
+#         post = Post.objects.get(learning_environment_id=1)
+#         self.assertIsInstance(post.learning_environment, Courses)
 
-# Create your tests here.
+#  Coud not make this one work, sorry Hocam. Betul:(
